99e608fc9b803917dc67519ab2b92bbe7d54f8d8
bhushan.jain@diaspark.com <bhushan.jain@diaspark.com>
2020-10-22 19:16:52 +0530

    First commit

---
+++ BMS/Controllers/MailController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+
+namespace WebApplication2.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("ekn.user.dp@gmail.com", "Book My Show");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "User@123";
+                    var sub = "Book My Show Ticket Confirmed";
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                         Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error mail not sent";
+               
+            }
+            return View();
+        }
+    }
+}
---
+++ BMS/Models/BookingHistoryModels.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class BookingHistoryModels
+    {
+        [JsonProperty(PropertyName = "SeatSelected")]
+        public string Selectseat { get; set; }
+
+        [JsonProperty(PropertyName = "Fname")]
+        public string Fname { get; set; }
+
+        [JsonProperty(PropertyName = "Email")]
+        public string InputEmail1 { get; set; }
+    }
+}
